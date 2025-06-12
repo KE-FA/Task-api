6dfdd62c2054b5d0725e63072bae98a4bb4aabbf
NeuraSync <kefakameti@gmail.com>
2025-06-12 21:10:20 +0300

    modify js file

--- index.js
+++ index.js
@@ -86,13 +86,7 @@ app.patch("/tasks/:id", async (req, res) => {
   }
 });
 
-let port;
-if (process.env.PORT) {
-  port = process.env.PORT;
-} else {
-  port = 5000;
-}
-
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
